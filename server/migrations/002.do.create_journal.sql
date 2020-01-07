fc2285627a47336c42c18326fde9bcd847757854
Maria Danielson <mal3905@gmail.com>
2020-01-07 13:34:26 -0500

    x

--- server/migrations/002.do.create_journal.sql
+++ server/migrations/002.do.create_journal.sql
@@ -1,6 +1,6 @@
 CREATE TABLE journal (
     id serial PRIMARY KEY, 
-    image (id int, img verbinary(max))
+    image INTEGER,
     name TEXT NOT NULL,
     date_created TIMESTAMP DEFAULT now() NOT NULL,
     location TEXT,
@@ -9,7 +9,7 @@ CREATE TABLE journal (
     rating INTEGER,
     abv INTEGER,
     heaviness INTEGER,
-    color INTEGER
+    color INTEGER,
     user_id INTEGER REFERENCES user(id)
         ON DELETE CASCADE NOT NULL
 );
