56c0a3d375848d4555866353201adb7d110517ac
Andrea Bender <andreab.1726@gmail.com>
2020-01-07 14:01:34 -0500

    changes to postgrator, etc

--- server/seeds/seed.tables.sql
+++ server/seeds/seed.tables.sql
@@ -13,6 +13,6 @@ VALUES
 INSERT INTO journal (id, image, name, location, description, rating, abv, heaviness, color, user_id)
 VALUES 
     (1, 1, 'double white', '123 abc street brooklyn, NY 11103', 'best beer ever!', 5, 4, 3, 2, 1),
-    (1, 1, 'Berry Triple lager', '123 xyz street bethesda, MD 20752', 'Not so great, tasteless!', 3, 5, 3, 4, 1);
+    (2, 1, 'Berry Triple lager', '123 xyz street bethesda, MD 20752', 'Not so great, tasteless!', 3, 5, 3, 4, 1);
 
 COMMIT; 
--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,8 @@
 /* eslint-disable strict */
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/dearbeer',
+	PORT: process.env.PORT || 8000,
+	NODE_ENV: process.env.NODE_ENV || 'development',
+	DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/dearbeer'
+	// JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+	// JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 };
