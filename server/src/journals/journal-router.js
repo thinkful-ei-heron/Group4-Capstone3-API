bc60a30ff888f727e4d1b1e8e25c137f1eaea1aa
Andrea Bender <andreab.1726@gmail.com>
2020-01-08 10:04:06 -0700

    working on GET

--- server/src/journals/journal-router.js
+++ server/src/journals/journal-router.js
@@ -6,3 +6,33 @@ const { requireAuth } = require('../middleware/jwt-auth');
 
 const journalRouter = express.Router();
 const jsonBodyParser = express.json();
+
+// const serializeJournals = {}
+journalRouter.route('/').all(requireAuth).get((req, res, next) => {
+	JournalService.getAllJournals(req.app.get('db'), req.user.id)
+		.then((journals) => {
+			res.json(journals);
+		})
+		.catch(next);
+});
+
+journalRouter
+	.route('/:id')
+	.all(requireAuth)
+	.all((req, res, next) => {
+		JournalService.getById(req.app.get('db'), req.params.id)
+			.then((journal) => {
+				if (!journal) {
+					return res.status(404).json({
+						error: { message: 'Journal does not exist' }
+					});
+				}
+				res.journal = journal;
+				next();
+				return null;
+			})
+			.catch(next);
+	})
+	.get((req, res, next) => {
+		res.json(res.goal);
+	});
