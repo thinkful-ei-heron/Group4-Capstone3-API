4ff42862b97886c17b00e1c90daad1e8af846f7b
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-16 14:16:48 -0700

    Update

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:123@localhost/dearbeer',
+	API_KEY: process.env.API_Key || 'AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g',
 	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
--- src/maps/maps-router.js
+++ src/maps/maps-router.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 const {requireAuth} = require('../middleware/jwt-auth');
-
+const  { API_KEY } = require('../config');
 const mapsRouter = express.Router();
 const jsonBodyParser = express.json();
 const path = require('path');
@@ -11,23 +11,33 @@ mapsRouter.route('/')
     .all(requireAuth)
     .post(jsonBodyParser, (req, res, next) => {
         const {zipcode} = req.body;
-        console.log(zipcode);
-        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&type=bar&key=AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g`)
-            .then((obj) => obj.json()).then(
-                (obj)=>{
-            if(obj.results.length > 0) {
-                let {lat, lng} = obj.results[0].geometry.location;
-                fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=20000&name=bar&key=AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g`)
-                    .then((bars)=> bars.json()).then((bars)=>{
-                    fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=20000&name=brewery&key=AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g`)
-                        .then((breweries)=> breweries.json()).then((breweries)=> res.json({results: obj.results[0].geometry.location, breweries: breweries.results, bars: bars.results}))
-
+        for (const field of [ 'zipcode']) {
+            if (!(field in req.body)) {
+                return res.status(400).json({
+                    error: {message: `Missing ${field} in request body`}
                 });
-
-            } else {
-                res.status(404).json({error: 'Location Not Found'})
             }
-                });
+        }
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&type=bar&key=${API_KEY}`)
+            .then((obj) => obj.json()).then(
+            (obj) => {
+                if (obj.results.length > 0) {
+                    let {lat, lng} = obj.results[0].geometry.location;
+                    fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&name=bar&key=${API_KEY}&rankby=distance`)
+                        .then((bars) => bars.json()).then((bars) => {
+                        fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&name=brewery&key=${API_KEY}&rankby=distance`)
+                            .then((breweries) => breweries.json()).then((breweries) => res.json({
+                            results: obj.results[0].geometry.location,
+                            breweries: breweries.results,
+                            bars: bars.results
+                        }))
+
+                    });
+
+                } else {
+                    res.status(404).json({error: 'Location Not Found'})
+                }
+            });
     });
 
 
